695141136e060c2a90006381bf6c1ac029b45263
Tylor Childers <inukai@ufl.edu>
2018-04-08 14:38:07 -0500

    Added policy and user tables

--- server/server.py
+++ server/server.py
@@ -1,11 +1,58 @@
 #!/usr/bin/python2
-import falcon, pymongo
+import flask
+import ujson
+import md5
 
-class RequestMonitor:
-    def on_get(self, req, resp):
-        resp.status = falcon.HTTP_200
-        resp.body = "TEST COMPLETE"
+def makeToken(name):
+    m = md5.new()
+    m.update(name)
+    return m.digest().encode('hex')
 
-app = falcon.API()
-santa = RequestMonitor()
-app.add_route('/test', santa)
+users = {}
+app = flask.Flask(__name__,static_folder='./static')
+
+users = None
+data = None
+policy = None
+
+with(open('../tables/users.json') as j):
+    users = ujson.loads(j.read())['users']
+
+with(open('../tables/data.json') as j):
+    data = ujson.loads(j.read())
+
+with(open('../tables/policy.json') as j):
+    policy = ujson.loads(j.read())
+
+@app.route('/<path:path>')
+def get_file(path):
+    return send_from_directory("./",path)
+
+@app.route('/login')
+def login():
+    try:
+        name = request.json["username"]
+        user = None
+        for u in users:
+            if u['username'] is name:
+                user = u
+                break
+        if user is None:
+            flask.abort(401)
+        users[makeToken(u['username'])] = u['UUID']
+        return users[u['UUID']]
+    except KeyError as e:
+        flask.abort(401)
+
+@app.route('/read')
+def santaRead():
+    try:
+        RID = request.json["RID"]
+        UUID = request.json["UUID"]
+        user = users[RID]
+        
+    except KeyError as e:
+        flask.abort(401)
+
+
+@app.route('/write')
--- server/start.sh
+++ server/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-gunicorn server:app
+export FLASK_APP=./server.py
+flask run --host=0.0.0.0
