43fd5d841604c23d9515c185271e1f9f4d8f2140
Juan Jauregui <juan@jauregui.co>
2018-04-08 23:41:46 -0400

    maybe

--- web_services/scripts/controllers/LoginController.js
+++ web_services/scripts/controllers/LoginController.js
@@ -5,13 +5,6 @@
 
 	app.controller('LoginController', function( $http, $log, $location, $scope, validate) {
 		/* If the user is signed in, redirect them to /home */
-		validate_session((is_logged_in)=>{
-			/* If the user is logged in, redirect to  home  */
-			if (is_logged_in) {
-				$location.path('/home');
-			}
-		});
-
 		$scope.formData = {
 			'email': null,
 			'password': null,
@@ -45,37 +38,5 @@
 				});
 			}
 		};
-
-		$scope.register = function() {
-			$location.path('/register');
-		};
-
-		function validate_session(callback) {
-			$http.get('http://ec2-18-220-239-17.us-east-2.compute.amazonaws.com:5000/api/session/validate', $scope.formData)
-				.success(function(data, status, headers, config) {
-					console.log(status);
-					console.log(data);
-					callback(true);
-				})
-			.error(function(data, status, headers, config) {
-				console.log(status);
-				console.log(data);
-				callback(false);
-			});
-		}
 	});
-
-
-	//  app.controller("LoginController", function ( $http, $log, $location, $scope, validate) {
-	//      $scope.login = function (loginForm) {
-	//          if(loginForm.$valid) {
-	//            validate.login($scope.firstname);
-	//        } else {alert("Please fill in login info!")}
-	//      };
-	//
-	//      $scope.register = function () {
-	//          $location.path('/register');
-	//      };
-	//
-	//  });
 }());
